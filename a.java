95bbd287dade6bd688c1dec19640fd39ccd6e066
heozhou1314 <herozhou1314@gmail.com>
2014-08-19 15:57:52 +0800

    add

--- a.java
+++ a.java
@@ -1,6 +1,6 @@
- Hello Java hello GIt!
-   insert a new line string 
-
+Hello Java vim GIt!
+  insert a vim line string 
 Creating a New branch is quick
 edit the same file 
 megre and change the same
+
---
+++ b.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  b.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月19日 15时54分26秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+int maini(char [] arg){
+	printf("hello world");
+}
